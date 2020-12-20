27fed32afa1e2c950ba42ef6daab71721b89142a
Noah Hatt <nhatt@uoguelph.ca>
2020-12-20 00:59:57 -0500

    Room class added, error occurs in ScrGame

--- core/src/house/game/rogue/Constants.java
+++ core/src/house/game/rogue/Constants.java
@@ -3,4 +3,11 @@ package house.game.rogue;
 public class Constants {
     public final static int MENU_SCREEN = 0;
     public final static int GAME_SCREEN = 1;
+    public final static int NORTH = 0;
+    public final static int SOUTH = 1;
+    public final static int EAST = 2;
+    public final static int WEST = 3;
+
+    public final static int SCREEN_WIDTH = 1000;
+    public final static int SCREEN_HEIGHT = 900;
 }
